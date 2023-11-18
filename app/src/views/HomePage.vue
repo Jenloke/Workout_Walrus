02be24c52f7d565160c9e117d8e5570e33a4376b
Jenloke Magbojos <mjenloke@gmail.com>
2023-11-19 03:09:50 +0800

    implement basic ionic select based on exercis parameters

--- app/src/views/HomePage.vue
+++ app/src/views/HomePage.vue
@@ -19,27 +19,94 @@
         <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
       </div> -->
       
-      <div>
-        <ion-title size="large">Muscle</ion-title>
-      </div>
+      <ion-item>
+        <!-- const type = ['cardio', 'olympic_weightlifting', 'plyometrics', 'powerlifting', 'strength', 'stretching', 'strongman']; -->
+        <ion-select label="Type" label-placement="fixed" placeholder="Cardio">
+          <ion-select-option value="cardio">Cardio</ion-select-option>
+          <ion-select-option value="olympic_weightlifting">Olympic Weightlifting</ion-select-option>
+          <ion-select-option value="plyometrics">Plyometrics</ion-select-option>
+          <ion-select-option value="powerlifting">Powerlifting</ion-select-option>
+          <ion-select-option value="strength">Strength</ion-select-option>
+          <ion-select-option value="stretching">Stretching</ion-select-option>
+          <ion-select-option value="strongman">Strongman</ion-select-option>
+        </ion-select>
+      </ion-item>
 
-      <div>
+      <!-- <ion-title size="large">Muscle</ion-title> -->
+      <ion-item>
+        <!-- const muscle = ['abdominals', 'abductors', 'adductors', 'biceps', 'calves', 'chest', 'forearms', 'glutes', 'hamstrings', 'lats', 'lower_back', 'middle_back', 'neck', 'quadriceps', 'traps', 'triceps']; -->
+        <ion-select label="Muscle" label-placement="fixed" placeholder="Select One">
+          <ion-select-option value="abdominals">Abdominals</ion-select-option>
+          <ion-select-option value="abductors">Abductors</ion-select-option>
+          <ion-select-option value="adductors">Adductors</ion-select-option>
+          <ion-select-option value="biceps">Biceps</ion-select-option>
+          <ion-select-option value="calves">Calves</ion-select-option>
+          <ion-select-option value="chest">Chest</ion-select-option>
+          <ion-select-option value="forearms">Forearms</ion-select-option>
+          <ion-select-option value="glutes">Glutes</ion-select-option>
+          <ion-select-option value="hamstrings">Hamstrings</ion-select-option>
+          <ion-select-option value="lats">Lats</ion-select-option>
+          <ion-select-option value="lower_back">Lower Back</ion-select-option>
+          <ion-select-option value="middle_back">Middle Back</ion-select-option>
+          <ion-select-option value="neck">Neck</ion-select-option>
+          <ion-select-option value="quadriceps">Quadriceps</ion-select-option>
+          <ion-select-option value="triceps">Triceps</ion-select-option>
+        </ion-select>
+      </ion-item>
+
+      <!-- <ion-list>
+        <ion-item>
+          <ion-select 
+            label="Difficulty" 
+            label-placement="fixed" 
+            placeholder="Beginner"
+            @ionChange="handleChange($event)"
+          >
+            <ion-select-option value="beginner">Beginner</ion-select-option>
+            <ion-select-option value="intermediate">Intermediate</ion-select-option>
+            <ion-select-option value="expert">Expert</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list> -->
+
+      <ion-list>
+        <ion-item>
+          <ion-select
+            aria-label="Fruit"
+            placeholder="Select fruit"
+            @ionChange="handleChange($event)"
+          >
+            <ion-select-option value="apples">Apples</ion-select-option>
+            <ion-select-option value="oranges">Oranges</ion-select-option>
+            <ion-select-option value="bananas">Bananas</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list>
+
+      <!-- <ion-item>
         <ion-title size="large">Difficulty</ion-title>
         <ion-radio-group value="beginner">
           <ion-radio value="beginner" label-placement="end" class="radio-choices">Beginner</ion-radio><br />
           <ion-radio value="intermediate" label-placement="end" class="radio-choices">Intermediate</ion-radio><br />
           <ion-radio value="expert" label-placement="end" class="radio-choices">Expert</ion-radio><br />
         </ion-radio-group>
-      </div>
+      </ion-item> -->
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonHeader, IonPage, IonRadio, IonRadioGroup, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { IonRadio, IonRadioGroup } from '@ionic/vue';
+import { IonItem, IonList, IonSelect, IonSelectOption } from '@ionic/vue';
+// import { IonItem, IonSelect, IonSelectOption } from '@ionic/vue';
 
-const type = ['cardio', 'olympic_weightlifting', 'plyometrics', 'powerlifting', 'strength', 'stretching', 'strongman'];
-const muscle = ['abdominals',  'abductors', 'adductors', 'biceps', 'calves', 'chest', 'forearms', 'glutes', 'hamstrings', 'lats', 'lower_back', 'middle_back', 'neck', 'quadriceps', 'traps', 'triceps'];
+import { defineComponent } from 'vue';
+
+// const type = ['cardio', 'olympic_weightlifting', 'plyometrics', 'powerlifting', 'strength', 'stretching', 'strongman'];
+const type = ['cardio', 'plyometrics', 'powerlifting', 'strength', 'stretching'];
+// const muscle = ['abdominals', 'abductors', 'adductors', 'biceps', 'calves', 'chest', 'forearms', 'glutes', 'hamstrings', 'lats', 'lower_back', 'middle_back', 'neck', 'quadriceps', 'traps', 'triceps'];
+const muscle = ['abdominals', 'biceps', 'calves', 'chest', 'forearms', 'glutes', 'hamstrings', 'lats', 'lower_back', 'middle_back', 'neck', 'quadriceps', 'triceps'];
 const difficulty = ['beginner', 'intermediate', 'expert'];
 
 let urlExercise = `https://api.api-ninjas.com/v1/exercises?type=${type[0]}&?muscle=${muscle[7]}&?difficulty${difficulty[0]}`;
@@ -52,13 +119,43 @@ async function getExercise(url = urlExercise) {
   });
   return response.json();
 }
-console.log(getExercise());
+// console.log(getExercise());
 
 // async function getUsers(url = "http://localhost:3000/users") {
 //   const response = await fetch(url);
 //   return response.json();
 // }
 // console.log(getUsers());
+
+defineComponent({
+  components: { IonItem, IonList, IonSelect, IonSelectOption },
+  methods: {
+    handleChange(ev: any) {
+      console.log('ionChange fired with value: ' + ev.detail.value);
+    },
+    handleCancel() {
+      console.log('ionCancel fired');
+    },
+    handleDismiss() {
+      console.log('ionDismiss fired');
+    }
+  }
+})
+
+defineComponent({
+    components: { IonItem, IonList, IonSelect, IonSelectOption },
+    methods: {
+      handleChange(ev: any) {
+        console.log('ionChange fired with value: ' + ev.detail.value);
+      },
+      handleCancel() {
+        console.log('ionCancel fired');
+      },
+      handleDismiss() {
+        console.log('ionDismiss fired');
+      },
+    },
+  });
 
 </script>
 
