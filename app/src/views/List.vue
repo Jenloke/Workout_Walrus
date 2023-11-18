0c7ea3128a55041d6b66759b14b7f27e39fbeaee
Jenloke Magbojos <mjenloke@gmail.com>
2023-11-19 04:34:21 +0800

    implement selected option from ionic select

--- app/src/views/List.vue
+++ app/src/views/List.vue
@@ -1,74 +1,38 @@
-<!-- <template>
-    <ion-page></ion-page>
-    <ion-list>
-      <ion-item>
-        <ion-select
-          aria-label="Fruit"
-          placeholder="Select fruit"
-          @ionChange="handleChange($event)"
-          @ionCancel="handleCancel()"
-          @ionDismiss="handleDismiss()"
-        >
-          <ion-select-option value="apples">Apples</ion-select-option>
-          <ion-select-option value="oranges">Oranges</ion-select-option>
-          <ion-select-option value="bananas">Bananas</ion-select-option>
-        </ion-select>
-      </ion-item>
-    </ion-list>
-  </template>
+<template>
+<ion-list>
+    <ion-item>
+    <ion-select
+        aria-label="Fruit"
+        placeholder="Select fruit"
+        @ionChange="handleChange($event)"
+        @ionCancel="handleCancel()"
+        @ionDismiss="handleDismiss()"
+    >
+        <ion-select-option value="apples">Apples</ion-select-option>
+        <ion-select-option value="oranges">Oranges</ion-select-option>
+        <ion-select-option value="bananas">Bananas</ion-select-option>
+    </ion-select>
+    </ion-item>
+</ion-list>
+</template>
   
-  <script setup lang="ts">
-    import { IonItem, IonList, IonSelect, IonSelectOption } from '@ionic/vue';
-    import { defineComponent } from 'vue';
-  
-    export default defineComponent({
-      components: { IonItem, IonList, IonSelect, IonSelectOption },
-      methods: {
-        handleChange(ev: any) {
-          console.log('ionChange fired with value: ' + ev.detail.value);
-        },
-        handleCancel() {
-          console.log('ionCancel fired');
-        },
-        handleDismiss() {
-          console.log('ionDismiss fired');
-        },
-      },
-    });
-  </script> -->
+<script>
+import { IonItem, IonList, IonSelect, IonSelectOption } from '@ionic/vue';
+import { defineComponent } from 'vue';
 
-  <template>
-    <ion-page>
-      <ion-content>
-        <ion-item>
-          <ion-label>Select an option:</ion-label>
-          <ion-select v-model="selectedOption" @ionChange="handleOptionChange">
-            <ion-select-option value="option1">Option 1</ion-select-option>
-            <ion-select-option value="option2">Option 2</ion-select-option>
-            <ion-select-option value="option3">Option 3</ion-select-option>
-          </ion-select>
-        </ion-item>
-        <ion-item>
-          <ion-label>Selected option:</ion-label>
-          <ion-note>{{ selectedOption }}</ion-note>
-        </ion-item>
-      </ion-content>
-    </ion-page>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        selectedOption: null,
-      };
-    },
+export default defineComponent({
+    components: { IonItem, IonList, IonSelect, IonSelectOption },
     methods: {
-      handleOptionChange(event) {
-        console.log('Selected option:', event.detail.value);
-        // You can perform additional actions here if needed
-      },
+    handleChange(ev) {
+        // extracts value
+        console.log('ionChange fired with value: ' + ev.detail.value);
     },
-  };
-  </script>
-  
+    handleCancel() {
+        console.log('ionCancel fired');
+    },
+    handleDismiss() {
+        console.log('ionDismiss fired');
+    },
+    },
+});
+</script>
