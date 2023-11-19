1a962fb07da736af9399cfa3bc2fe6d2a200d891
Seth Arkin Cruzat <sethscruzat@gmail.com>
2023-11-20 01:24:43 +0800

    integrating on diff branch, check nio before merge

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,16 +1,48 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 // import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
+import TabsPage from '../views/TabsPage.vue'
 
 const routes = [
-  {
+/*   {
     path: '/',
     redirect: '/home'
   },
-  {
+/*   {
     path: '/home',
     name: 'Home',
     component: HomePage
+  } ,*/
+  {
+    path: '/',
+    redirect: '/tabs/tab1'
+  },
+  {
+    path: '/tabs/',
+    component: TabsPage,
+    children: [
+      {
+        path: '',
+        redirect: '/tabs/tab1'
+      },
+      {
+        path: 'tab1',
+        component: () => import('@/views/Tab1Page.vue')
+      },
+      {
+        path: 'tab2',
+        component: () => import('@/views/Tab2Page.vue')
+      },
+      {
+        path: 'tab3',
+        component: () => import('@/views/Tab3Page.vue')
+      },
+      {
+        path: 'home',
+        name: 'Home',
+        component: HomePage
+      }
+    ]
   }
 ]
 
