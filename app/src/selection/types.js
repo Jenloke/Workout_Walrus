0afd0bed094ddaefcc449c5fd4be25b3f0fc755e
Jenloke Magbojos <mjenloke@gmail.com>
2023-11-20 16:49:56 +0800

    separate options from homepage vue

---
+++ app/src/selection/types.js
@@ -0,0 +1,114 @@
+const types = [ 
+  { 
+    name: 'Cardio', 
+    value: 'cardio'
+  },
+  { 
+    name: 'Olympic Weightlifting', 
+    value: 'olympic_weightlifting'
+  },
+  { 
+    name: 'Plyometrics', 
+    value: 'plyometrics'
+  },
+  { 
+    name: 'Powerlifting', 
+    value: 'powerlifting'
+  },
+  { 
+    name: 'Strength', 
+    value: 'strength'
+  },
+  { 
+    name: 'Stretching', 
+    value: 'stretching'
+  },
+  { 
+    name: 'Strongman', 
+    value: 'strongman'
+  }
+]
+
+const muscles = [
+  {
+    name: 'Abdominals', 
+    value: 'abdominals'  
+  },
+  {
+    name: 'Abductors', 
+    value: 'abductors'  
+  },
+  {
+    name: 'Adductors', 
+    value: 'adductors'  
+  },
+  {
+    name: 'Biceps', 
+    value: 'biceps'  
+  },
+  {
+    name: 'Calves', 
+    value: 'calves'  
+  },
+  {
+    name: 'Chest', 
+    value: 'chest'  
+  },  
+  {
+    name: 'Forearms', 
+    value: 'forearms'
+  }, 
+  {
+    name: 'Glutes',
+    value: 'glutes'
+  },
+  {
+    name: 'Hamstrings',
+    value: 'hamstrings'
+  },
+  {
+    name: 'Lats',
+    value: 'lats'
+  },
+  {
+    name: 'Lower Back',
+    value: 'lower_back'
+  },
+  {
+    name: 'Middle Back',
+    value: 'middle_back'
+  },
+  {
+    name: 'Neck',
+    value: 'neck'
+  },
+  {
+    name: 'Quadriceps',
+    value: 'quadriceps'
+  },
+  {
+    name: 'Traps',
+    value: 'traps'
+  },
+  {
+    name: 'Triceps',
+    value: 'triceps'
+  } 
+];
+
+const difficulties = [
+  {
+    name: 'Beginner',
+    value: 'beginner'
+  }, 
+  {
+    name: 'Intermediate',
+    value: 'intermediate' 
+  }, 
+  {
+    name: 'Expert',
+    value: 'expert'
+  }
+];
+
+export { types, muscles, difficulties };
